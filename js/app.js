8960aebbd992883a7ee5067fde86c21931004f6b
Lisa Dwyer <mail@lisadwyer.de>
2018-05-20 10:08:41 +0200

    commit for tutor sesscion

--- js/app.js
+++ js/app.js
@@ -1,33 +1,104 @@
+//Variables
+
+const startButton = document.querySelector("#startButton");
+const board = document.querySelector("#board");
 /*
  * Create a list that holds all of your cards
- */
- const cardsList = [“fa fa-diamond”, “fa fa-paper-plane-o”, “fa fa-anchor”, “fa fa-bolt”,
- “fa fa-cube”, “fa fa-anchor”, “fa fa-leaf”, “fa fa-bicycle”, “fa fa-diamond”, “fa fa-bomb”,
- “fa fa-leaf”, “fa fa-bomb”, “fa fa-bolt”, “fa fa-bicycle”, “fa fa-paper-plane-o”, “fa fa-cube”];
+  */
+const cardPictures = document.getElementsByTagName("i");
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+const arrayOfCardPictures = Array.from(cardPictures);
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+arrayOfCardPictures.splice(0,4);
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+let shuffledCards = shuffle(arrayOfCardPictures);
 
-    return array;
+
+// FUNCTIONS:
+
+
+
+// // Create unordered list;
+//
+ function makeUL() {
+     // Create the deck element:
+
+     const deck = document.createElement("ul");
+     deck.classList.add("deck");
+     deck.setAttribute("id", "memory-board");
+
+     for(let j = 0; j < 16 ; j++) {
+         // Create the deck card:
+         const card = document.createElement("li");
+         //adding card class to card li:
+         card.classList.add("card");
+
+         //console.log(card);
+         // Set its contents:
+         card.appendChild(shuffledCards[j]);
+
+         // Add it to the list:
+         deck.appendChild(card);
+
+        }
+
+     // Finally, return the constructed list:
+     return deck;
+
+ }
+
+ /*
+  * Display the cards on the page
+  *   - shuffle the list of cards using the provided "shuffle" method below
+  *   - loop through each card and create its HTML
+  *   - add each card's HTML to the page
+  */
+
+  function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      while (currentIndex !== 0) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+  }
+
+function setupGame() {
+
+  for (let j = 0; j < 16; j++) {
+    shuffledCards[j].classList.remove("show", "open", "match");
+
+  makeUL();
+
+  }
+
 }
 
 
+function clickOnCard() {
+  for (let shuffledCard of shuffledCards) {
+  shuffledCard.addEventListener('click', showCard)
+}
+}
+
+
+function showCard () {
+  this.classList.add("open");
+  this.classList.add("show");
+
+}
+//
+//document.body.onload = setupGame();
+// $(document).ready(function() {
+//
+//   setupGame();
+//
+//     )}
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
